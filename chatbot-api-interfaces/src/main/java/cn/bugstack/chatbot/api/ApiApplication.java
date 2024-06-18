9516b313f90f4f45e8c39af73ce92b79c89e62ce
yujiao <961876584@qq.com>
2024-06-18 13:57:52 +0800

    初始化工程

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: ApiApplication
+ * Package: cn.bugstack.chatbot.api
+ * Description:
+ *
+ * @Author 佼佼
+ * @Create 2024/6/18 13:44
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,13 @@
+package cn.bugstack.chatbot.api.test;
+
+/**
+ * ClassName: ApiTest
+ * Package: cn.bugstack.chatbot.api.test
+ * Description:
+ *
+ * @Author 佼佼
+ * @Create 2024/6/18 13:51
+ * @Version 1.0
+ */
+public class ApiTest {
+}
